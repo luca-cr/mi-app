c8f13437e1d16af792e345ec405f9bbae750ffee
Lucas-Cruz <lucasnote6@gmail.com>
2023-06-30 11:48:13 -0300

    app terminada

--- src/App.js
+++ src/App.js
@@ -1,23 +1,87 @@
 import logo from './logo.svg';
 import './App.css';
+import {Testimonio} from './componentes/Testimonio.js';
+import {Coso} from './componentes/Testimonio';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code className='code'>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='contenedor-principal'>
+      <h1>Esto es lo que dicen nuestros alumnos sobre FreeCodeCamp</h1>
+      <Testimonio 
+        nombre='Lucas'
+        imagen='foto2'
+        pais='Argentina'
+        cargo='Ninguno'
+        empresa='Desocupado'
+        texto='ni ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani idea'
+        descripcion-imagen='dd' />
+
+        <Testimonio 
+          nombre='Sole'
+          imagen='foto1'
+          pais='Mendoza'
+          cargo='Cargo'
+          empresa='Empresa'
+          texto='ni ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani idea'
+          descripcion-imagen='coso cosoo'
+         />
+         <Testimonio 
+          nombre='Sole'
+          imagen='foto1'
+          pais='Mendoza'
+          cargo='Cargo'
+          empresa='Empresa'
+          texto='ni ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani idea'
+          descripcion-imagen='coso cosoo'
+         />
+         <Testimonio 
+          nombre='Sole'
+          imagen='foto1'
+          pais='Mendoza'
+          cargo='Cargo'
+          empresa='Empresa'
+          texto='ni ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani idea'
+          descripcion-imagen='coso cosoo'
+         />
+         <Testimonio 
+          nombre='Sole'
+          imagen='foto1'
+          pais='Mendoza'
+          cargo='Cargo'
+          empresa='Empresa'
+          texto='ni ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani idea'
+          descripcion-imagen='coso cosoo'
+         />
+         <Testimonio 
+          nombre='Sole'
+          imagen='foto1'
+          pais='Mendoza'
+          cargo='Cargo'
+          empresa='Empresa'
+          texto='ni ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani idea'
+          descripcion-imagen='coso cosoo'
+         />
+         <Testimonio 
+          nombre='Sole'
+          imagen='foto1'
+          pais='Mendoza'
+          cargo='Cargo'
+          empresa='Empresa'
+          texto='ni ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani idea'
+          descripcion-imagen='coso cosoo'
+         />
+         <Testimonio 
+          nombre='Sole'
+          imagen='foto1'
+          pais='Mendoza'
+          cargo='Cargo'
+          empresa='Empresa'
+          texto='ni ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani ideani idea'
+          descripcion-imagen='coso cosoo'
+         />
+         
+      </div>
     </div>
   );
 }
